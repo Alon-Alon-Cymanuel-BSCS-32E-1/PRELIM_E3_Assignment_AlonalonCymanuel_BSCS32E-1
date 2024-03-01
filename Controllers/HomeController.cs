83b9e3e2d04b8fd7b63b728de008ccf0ed934c0e
Cymanuel Alon-Alon <c2874-23@itmlyceumalabang.onmicrosoft.com>
2024-03-02 00:10:36 +0800

    PRELIM_E3_Assignment_ALONALONCYMANUEL_BSCS32E_1

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PRELIM_E3_Assignment_AlonalonCymanuel_BSCS32E_1.Models;
+
+namespace PRELIM_E3_Assignment_AlonalonCymanuel_BSCS32E_1.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult School()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PRELIM_E3_Assignment_AlonalonCymanuel_BSCS32E_1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
